47faa72c5f8f4cad32bc0c1e3a51515a5db5e2cd
solidados <geekbrains.pk@internet.ru>
2023-04-26 22:47:56 +0400

    feat: add modal and burger script

--- js/accordion.js
+++ js/accordion.js
@@ -1,20 +1,33 @@
-const btns = document.querySelectorAll(".feature__link");
 const lists = document.querySelectorAll(".feature-sub");
+const btns = document.querySelectorAll(".feature__link");
+
+const openTab = (tab, idx) => {
+  tab.classList.add('feature__link_active');
+  lists[idx].classList.remove('hidden');
+}
+
+const closeTab = (tab, idx) => {
+  tab.classList.remove('feature__link_active');
+  lists[idx].classList.add('hidden');
+}
 
 btns.forEach((btnItem, index) => {
-  btnItem.addEventListener("click", (event) => {
-    // const targetElement = event.target;
+  btnItem.addEventListener("click", () => {
+    if (btnItem.classList.contains('feature__link_active')) {
+      closeTab(btnItem, index);
+    } else {
+      openTab(btnItem, index);
+    }
 
-    btns.forEach((btnItem) => {
-      // if (targetElement.closest(".feature__link")) {}
-        btnItem.classList.remove("feature__link_active");
+    btns.forEach((btnItem, idx) => {
+      if (index !== idx) {
+        btnItem.classList.remove('feture__link_active');
+      }
     });
-    btnItem.classList.add("feature__link_active");
-
-    lists.forEach((listItem) => {
-      listItem.classList.add("hidden");
-    });
-
-    lists[index].classList.remove("hidden");
+    lists.forEach((listItem, idx) => {
+      if (index !== idx) {
+        listItem.classList.add('hidden');
+      }
+    })
   });
 });
